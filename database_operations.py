5c54297c8db7bc99d54349ed0f87780fe4b345d5
Ruslan <yakovchik.ruslan@gmail.com>
2020-03-01 21:44:10 +0300

    Added functions for selecting data from tables.

--- database_operations.py
+++ database_operations.py
@@ -38,6 +38,63 @@ class DataBase:
         self.connect.commit()
 
 
+    #____________________________________________________________________________________--
+
+    def rooms_list_and_count_students_inside(self):
+        self.cursor.execute(
+            """
+            SELECT  rooms.id , rooms.name , COUNT(students.id) as students
+            FROM rooms
+            JOIN students ON rooms.id = students.room_id
+            GROUP BY rooms.id
+            """
+        )
+        result = self.cursor.fetchall()
+        return result
+
+    def top_5_rooms_with_smallest_average_age(self):
+        self.cursor.execute(
+            """
+            SELECT rooms.id , rooms.name , AVG(TIMESTAMPDIFF(YEAR, students.birthday,NOW())) as average_age
+            FROM rooms
+            JOIN students ON rooms.id = students.room_id
+            GROUP BY rooms.id 
+            ORDER BY average_age
+            LIMIT 5
+            """
+        )
+        result = self.cursor.fetchall()
+        return result
+
+    def top5_rooms_with_biggest_age_difference(self):
+        self.cursor.execute(
+            """
+            SELECT rooms.id , rooms.name , TIMESTAMPDIFF(YEAR , MAX(students.birthday) , MIN(students.birthday)) as max_diff
+            FROM rooms
+            JOIN students ON rooms.id = students.room_id
+            GROUP BY rooms.id
+            ORDER BY max_diff DESC
+            LIMIT 5
+            """
+        )
+        result = self.cursor.fetchall()
+        return result
+
+    def rooms_list_with_different_sex(self):
+        self.cursor.execute(
+            """
+            SELECT DISTINCT rooms.id , rooms.name , COUNT(DISTINCT students.sex) as sex
+            FROM rooms
+            JOIN students ON rooms.id = students.room_id
+            GROUP BY rooms.id 
+            HAVING sex = 2
+            """
+        )
+        result = self.cursor.fetchall()
+        return result
+    #____________________________________________________________________________________++
+
+
 sql_create_databases = [
     """CREATE SCHEMA IF NOT EXISTS task4_new_db DEFAULT CHARACTER SET utf8;""",
     ]
